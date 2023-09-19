ef1cb548329cab4bbc426380c7d48fe78a2b94cf
besoeasy <besoeasy@users.noreply.github.com>
2023-09-20 01:42:47 +0530

    build(deps): update default.vue, package-lock.json and contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -7,10 +7,16 @@
 
 			<div class="m-auto p-8">
 				<div class="my-10">
-					<p class="mb-4 text-2xl">For inquiries, email us:</p>
+					<p class="mb-4 text-2xl">Have questions? Email us:</p>
 					<a href="mailto:moonsyntax@proton.me" class="text-blue-500 hover:underline text-xl"
 						>moonsyntax@proton.me</a
 					>
+				</div>
+
+				<div class="my-4">
+					<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+						<a href="https://calendly.com/moonsyntax" target="_blank">Schedule a Meeting</a>
+					</button>
 				</div>
 			</div>
 		</section>
