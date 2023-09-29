380ade17c90095760274ca8a77eb363c7cc43940
besoeasy <besoeasy@users.noreply.github.com>
2023-09-29 06:16:40 +0530

    update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
 				<div class="flex items-center justify-center h-10 py-10 uppercase lg:text-lg font-bold">
 					<div class="flex items-center space-x-4">
 						<NuxtLink v-if="!isIndexPage" to="/" class="">
-							<Synb textinput=" 🡸 " />
+							<Synb textinput="HOME" />
 						</NuxtLink>
 						<a href="https://blog.moonsyntax.com/">BLOG</a>
 						<NuxtLink to="/team">Team</NuxtLink>
