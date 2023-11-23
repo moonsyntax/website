6d7d600bf6cd39d05e725793eb6aa5d4cd050f48
besoeasy <besoeasy@users.noreply.github.com>
2023-11-23 12:58:19 +0530

    update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,112 +1,106 @@
 <template>
   <div>
     <section class="py-20 md:py-32 overflow-hidden">
-      <div class="m-auto container">
-        <div class="max-w-2xl text-center mx-auto mb-20">
-          <span
-            class="inline-block py-1 px-3 mb-4 text-xs font-semibold text-orange-900 bg-orange-50 rounded-full"
-            >Feel free to contact us.</span
-          >
-          <h1
-            class="font-heading text-5xl xs:text-6xl font-bold text-gray-900 mb-4"
-          >
-            <span>Let’s stay</span>
-            <span class="font-serif italic">connected</span>
-          </h1>
-          <p class="text-xl text-gray-500 font-semibold">
-            We are always open to new ideas and collaborations.
-          </p>
-        </div>
-        <div class="max-w-md lg:max-w-6xl mx-auto">
-          <div class="flex flex-wrap">
-            <div class="w-full lg:w-1/2 px-4 order-last lg:order-first">
-              <Animation loc="contact" />
+      <div class="max-w-2xl text-center mx-auto mb-20">
+        <span
+          class="inline-block py-1 px-3 mb-4 text-xs font-semibold text-orange-900 bg-orange-50 rounded-full"
+          >Feel free to contact us.</span
+        >
+        <h1
+          class="font-heading text-5xl xs:text-6xl font-bold text-gray-900 mb-4"
+        >
+          <span>Let’s stay</span>
+          <span class="font-serif italic">connected</span>
+        </h1>
+        <p class="text-xl text-gray-500 font-semibold">
+          We are always open to new ideas and collaborations.
+        </p>
+      </div>
+      <div class="m-auto max-w-6xl p-5">
+        <div class="flex flex-wrap">
+          <div class="m-auto w-full lg:w-1/2 order-last lg:order-first">
+            <Animation loc="contact" />
+          </div>
+          <div class="m-auto w-full lg:w-1/2">
+            <div class="flex mb-12 items-center">
+              <div
+                class="flex flex-shrink-0 mr-5 sm:mr-8 items-center justify-center p-1 w-16 sm:w-20 h-16 sm:h-20 rounded-full bg-blue-200"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
+                  />
+                </svg>
+              </div>
+              <div>
+                <span class="text-lg text-gray-500">Telegram Group</span>
+                <span class="block text-lg font-semibold text-gray-900">
+                  <a
+                    href="https://t.me/moonsyntax"
+                    class="text-blue-400 hover:text-blue-600 text-2xl"
+                    >@moonsyntax</a
+                  ></span
+                >
+              </div>
             </div>
-            <div class="w-full lg:w-1/2 px-4 mb-12 lg:mb-0">
-              <div class="max-w-md py-6 lg:ml-auto">
-                <div class="flex mb-12 items-center">
-                  <div
-                    class="flex flex-shrink-0 mr-5 sm:mr-8 items-center justify-center p-1 w-16 sm:w-20 h-16 sm:h-20 rounded-full bg-blue-200"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="w-6 h-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-                      />
-                    </svg>
-                  </div>
-                  <div>
-                    <span class="text-lg text-gray-500">Telegram Group</span>
-                    <span class="block text-lg font-semibold text-gray-900">
-                      <a
-                        href="https://t.me/moonsyntax"
-                        class="text-blue-400 hover:text-blue-600 text-2xl"
-                        >@moonsyntax</a
-                      ></span
-                    >
-                  </div>
-                </div>
-                <div class="flex mb-12 items-center">
-                  <div
-                    class="flex flex-shrink-0 mr-5 sm:mr-8 items-center justify-center p-1 w-16 sm:w-20 h-16 sm:h-20 rounded-full bg-yellow-200"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="w-6 h-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
-                      />
-                    </svg>
-                  </div>
-                  <div>
-                    <span class="text-lg text-gray-500">Email Manager</span>
-                    <span class="block text-lg font-semibold text-gray-900"
-                      >moonsyntax@proton.me</span
-                    >
-                  </div>
-                </div>
-                <div class="flex items-center">
-                  <div
-                    class="flex flex-shrink-0 mr-5 sm:mr-8 items-center justify-center p-1 w-16 sm:w-20 h-16 sm:h-20 rounded-full bg-green-200"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="w-6 h-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
-                      />
-                    </svg>
-                  </div>
-                  <div>
-                    <span class="text-lg text-gray-500"
-                      >Email Organisation</span
-                    >
-                    <span class="block text-lg font-semibold text-gray-900"
-                      >{{ email }}@moonsyntax.com</span
-                    >
-                  </div>
-                </div>
+            <div class="flex mb-12 items-center">
+              <div
+                class="flex flex-shrink-0 mr-5 sm:mr-8 items-center justify-center p-1 w-16 sm:w-20 h-16 sm:h-20 rounded-full bg-yellow-200"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
+                  />
+                </svg>
+              </div>
+              <div>
+                <span class="text-lg text-gray-500">Email Manager</span>
+                <span class="block text-lg font-semibold text-gray-900"
+                  >moonsyntax@proton.me</span
+                >
+              </div>
+            </div>
+            <div class="flex mb-12 items-center">
+              <div
+                class="flex flex-shrink-0 mr-5 sm:mr-8 items-center justify-center p-1 w-16 sm:w-20 h-16 sm:h-20 rounded-full bg-green-200"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M2.25 13.5h3.86a2.25 2.25 0 012.012 1.244l.256.512a2.25 2.25 0 002.013 1.244h3.218a2.25 2.25 0 002.013-1.244l.256-.512a2.25 2.25 0 012.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 00-2.15-1.588H6.911a2.25 2.25 0 00-2.15 1.588L2.35 13.177a2.25 2.25 0 00-.1.661z"
+                  />
+                </svg>
+              </div>
+              <div>
+                <span class="text-lg text-gray-500">Email Organisation</span>
+                <span class="block text-lg font-semibold text-gray-900"
+                  >{{ email }}@moonsyntax.com</span
+                >
               </div>
             </div>
           </div>
