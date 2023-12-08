5816a7f4481c9fcdaa905a843b88d93a34fb82e6
besoeasy <besoeasy@users.noreply.github.com>
2023-12-08 14:15:33 +0530

    Update hover animation duration and scale in Anim.vue

--- components/Anim.vue
+++ components/Anim.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hover:transform hover:scale-110 hover:transition-transform hover:duration-800">
+  <div class="hover:transform hover:scale-105 hover:transition-transform hover:duration-500">
     <lottie-player :src="loc" background="transparent" loop autoplay class="w-full h-full"></lottie-player>
   </div>
 </template>
