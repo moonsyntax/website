3f6fea40d639a4fa780596243b1e7c48c7c4314e
besoeasy <besoeasy@users.noreply.github.com>
2023-12-12 15:05:25 +0530

    Remove unnecessary link to work page and update social media links and text formatting in app.vue and index.vue

--- app.vue
+++ app.vue
@@ -10,7 +10,6 @@ const route = useRoute()
         <div class="flex items-center justify-center uppercase text-base lg:text-xl space-x-4 lg:space-x-8 font-bold">
           <NuxtLink v-if="route.path !== '/'" to="/" class="hover:scale-110 ">HOME</NuxtLink>
           <NuxtLink to="/service" class="hover:scale-110 ">Services</NuxtLink>
-          <NuxtLink to="/work" class="hover:scale-110 ">work</NuxtLink>
           <NuxtLink to="/about" class="hover:scale-110 ">about</NuxtLink>
           <NuxtLink to="/contact" class="hover:scale-110 ">CONTACT</NuxtLink>
         </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,34 @@
 <template>
-  <div class="m-auto max-w-7xl p-4">
-    <div class="flex flex-wrap items-center -m-6 pt-32 pb-36">
-      <div class="w-full lg:w-1/2 p-6">
-        <h1 class="mb-6 font-heading text-7xl md:text-10xl xl:text-12xl text-gray-900 font-bold">
-          MOON SYNTAX </h1>
-        <p class="mb-9 text-gray-600 text-lg">
-          The decentralized organization that's shaking up the world of
-          blockchain startups. With a focus on developing, managing, and
-          funding cutting-edge projects, our team of experts delivers
-          unparalleled support and guidance.
-        </p>
+	<div class="m-auto max-w-7xl p-4">
+		<div class="flex flex-wrap items-center -m-6 pt-32 pb-36">
+			<div class="w-full lg:w-1/2 p-6">
+				<h1 class="mb-6 font-heading text-7xl md:text-10xl xl:text-12xl text-gray-900 font-bold">MOON SYNTAX</h1>
+				<p class="mb-9 text-gray-600 text-lg">
+					The decentralized organization that's shaking up the world of blockchain startups. With a focus on developing, managing, and funding cutting-edge projects, our team of experts delivers
+					unparalleled support and guidance.
+				</p>
 
-        <div class="pt-3 space-y-3 space-y-4 leading-6 text-left xl:pt-3 xl:space-y-5 uppercase">
-          <div class="relative inline-block mr-3">
-            <a href="https://twitter.com/moonsyntax">
-              <butx textinput="Twitter" />
-            </a>
-          </div>
-          <div class="relative inline-block mr-3">
-            <a href="https://medium.com/moonsyntax">
-              <butx textinput="Medium" />
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="w-full lg:w-1/2 p-6">
-        <Anim loc="/anim/home.json" /> 
-      </div>
-    </div>
-  </div>
+				<div class="pt-3 space-y-3 space-y-4 leading-6 text-left xl:pt-3 xl:space-y-5 uppercase">
+					<div class="relative inline-block mr-3">
+						<a href="https://twitter.com/moonsyntax">
+							<butx textinput="Twitter" />
+						</a>
+					</div>
+					<div class="relative inline-block mr-3">
+						<a href="https://medium.com/moonsyntax">
+							<butx textinput="Blog" />
+						</a>
+					</div>
+					<div class="relative inline-block mr-3">
+						<a href="https://medium.com/moonsyntaxwork">
+							<butx textinput="Our Work" />
+						</a>
+					</div>
+				</div>
+			</div>
+			<div class="w-full lg:w-1/2 p-6">
+				<Anim loc="/anim/home.json" />
+			</div>
+		</div>
+	</div>
 </template>
