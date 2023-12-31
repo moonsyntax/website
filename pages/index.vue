c995f213974822da53bb7e6f606475efc672b86f
besoeasy <besoeasy@users.noreply.github.com>
2023-12-31 08:50:18 +0530

    Refactor button component and update links in index page

--- pages/index.vue
+++ pages/index.vue
@@ -4,25 +4,20 @@
 			<div class="w-full lg:w-1/2 p-6">
 				<h1 class="mb-6 font-heading text-7xl md:text-10xl xl:text-12xl text-gray-900 font-bold">MOON SYNTAX</h1>
 				<p class="mb-9 text-gray-600 text-lg">
-					The decentralized organization that's shaking up the world of blockchain startups. With a focus on developing, managing, and funding cutting-edge projects, our team of experts delivers
-					unparalleled support and guidance.
+					The decentralized organization that's shaking up the world of blockchain startups. With a focus on
+					developing, managing, and funding cutting-edge projects, our team of experts delivers unparalleled
+					support and guidance.
 				</p>
 
 				<div class="pt-3 space-y-3 space-y-4 leading-6 text-left xl:pt-3 xl:space-y-5 uppercase">
 					<div class="relative inline-block mr-3">
-						<a href="https://twitter.com/moonsyntax">
-							<butx textinput="Twitter" />
-						</a>
+						<butx textinput="Twitter" link="https://twitter.com/moonsyntax" />
 					</div>
 					<div class="relative inline-block mr-3">
-						<a href="https://medium.com/moonsyntax">
-							<butx textinput="Blog" />
-						</a>
+						<butx textinput="Blog" link="https://medium.com/moonsyntax" />
 					</div>
 					<div class="relative inline-block mr-3">
-						<a href="https://medium.com/moonsyntaxwork">
-							<butx textinput="Our Work" />
-						</a>
+						<butx textinput="Our Work" link="https://medium.com/moonsyntaxwork" />
 					</div>
 				</div>
 			</div>
