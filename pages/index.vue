3306f90d382870f85df34c697e44160139d88594
besoeasy <besoeasy@users.noreply.github.com>
2023-12-31 10:11:41 +0530

    Update index.vue styling and content

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,16 @@
 	<div class="m-auto max-w-7xl p-4">
 		<div class="flex flex-wrap items-center -m-6 pt-32 pb-36">
 			<div class="w-full lg:w-1/2 p-6">
-				<span class="text-2xl font-semibold bg-yellow-200 text-gray-600 rounded-2xl px-5 py-3 mb-10"
-					>x@moonsyntax.com</span
-				>
+				<span class="text-sm font-semibold bg-yellow-200 text-gray-600 rounded-2xl px-4 py-2">x@moonsyntax.com</span>
 
-				<h1 class="my-8 text-7xl md:text-10xl xl:text-12xl text-gray-900 font-bold">MOON SYNTAX</h1>
-				<p class="my-6 text-gray-600 text-xl">
+				<h1 class="my-6 text-3xl xl:text-7xl text-gray-800 font-bold">MOON SYNTAX</h1>
+				<p class="my-6 text-xl lg:text-2xl tracking-wide">
 					The decentralized organization that's shaking up the world of blockchain startups. With a focus on
 					developing, managing, and funding cutting-edge projects, our team of experts delivers unparalleled
 					support and guidance.
 				</p>
 
-				<div class="pt-3 leading-6 text-left uppercase">
+				<div class="pt-5 leading-6 uppercase font-semibold">
 					<div class="relative inline-block mr-3">
 						<butx textinput="Twitter" link="https://twitter.com/moonsyntax" />
 					</div>
