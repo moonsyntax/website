b3721b61785752971195628aa5b4bf9e7d3bc4ba
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-01-06 13:31:30 +0530

    removed email from index page

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,6 @@
 	<div class="m-auto max-w-7xl p-4">
 		<div class="flex flex-wrap items-center -m-6 pt-32 pb-36">
 			<div class="w-full lg:w-1/2 p-6">
-				<span class="text-sm font-semibold bg-yellow-200 text-gray-600 rounded-2xl px-4 py-2">x@moonsyntax.com</span>
-
 				<h1 class="my-6 text-3xl xl:text-7xl text-gray-800 font-bold">MOON SYNTAX</h1>
 				<p class="my-6 text-xl lg:text-2xl tracking-wide">
 					The decentralized organization that's shaking up the world of blockchain startups. With a focus on
