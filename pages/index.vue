e5c5c9add29cc32e9fe2da12589feb406ece1cd3
besoeasy <besoeasy@users.noreply.github.com>
2024-01-21 06:14:50 +0530

    Add animation to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="flex flex-wrap items-center">
 		<div class="w-full lg:w-1/2">
+			<Anim loc="/anim/home.json" />
+		</div>
+
+		<div class="w-full lg:w-1/2">
 			<h1 class="my-6 text-2xl lg:text-6xl font-bold text-gray-800 p-6">MOON { SYNTAX }</h1>
 			<p class="my-6 text-lg lg:text-2xl tracking-wide rounded-2xl bg-yellow-200 p-6">
 				The decentralized organization that's shaking up the world of blockchain startups. With a focus on developing,
@@ -15,9 +19,6 @@
 					<butx textinput="Blog" link="https://medium.com/moonsyntax" />
 				</div>
 			</div>
-		</div>
-		<div class="w-full lg:w-1/2">
-			<Anim loc="/anim/home.json" />
 		</div>
 	</div>
 </template>
