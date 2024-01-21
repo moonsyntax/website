991b060a5bd22d31f3fb677bf2d2f863644d8f00
besoeasy <besoeasy@users.noreply.github.com>
2024-01-21 06:41:17 +0530

    Refactor app and index pages

--- app.vue
+++ app.vue
@@ -1,21 +1,27 @@
-<script setup>
-	const route = useRoute();
-</script>
-
 <template>
-	<div class="m-auto p-2 max-w-7xl">
-		<div class="font-bold uppercase text-gray-600 hover:text-gray-800">
-			<div class="m-auto container py-6 px-3">
-				<div class="flex items-center justify-center lg:text-xl space-x-4 lg:space-x-8">
-					<NuxtLink v-if="route.path !== '/'" to="/" class="hover:scale-110">HOME</NuxtLink>
-					<NuxtLink to="/service" class="hover:scale-110">Services</NuxtLink>
-					<NuxtLink to="/about" class="hover:scale-110">About</NuxtLink>
-					<a target="_blank" href="https://medium.com/moonsyntaxwork" class="hover:scale-110">Work</a>
-					<NuxtLink to="/contact" class="hover:scale-110">CONTACT</NuxtLink>
+	<div :style="{ backgroundImage: `url(${wave1})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }" class="min-h-screen">
+		<div class="m-auto p-2 max-w-7xl">
+			<div class="font-bold uppercase text-gray-600 hover:text-gray-800">
+				<div class="m-auto container py-6 px-3">
+					<div class="flex items-center justify-center lg:text-xl space-x-4 lg:space-x-8">
+						<NuxtLink v-if="route.path !== '/'" to="/" class="hover:scale-110">HOME</NuxtLink>
+						<NuxtLink to="/service" class="hover:scale-110">Services</NuxtLink>
+						<NuxtLink to="/about" class="hover:scale-110">About</NuxtLink>
+						<a target="_blank" href="https://medium.com/moonsyntaxwork" class="hover:scale-110"
+							>Work</a
+						>
+						<NuxtLink to="/contact" class="hover:scale-110">CONTACT</NuxtLink>
+					</div>
 				</div>
 			</div>
-		</div>
 
-		<NuxtPage />
+			<NuxtPage />
+		</div>
 	</div>
 </template>
+
+<script setup>
+	const route = useRoute();
+
+	import wave1 from '@/public/blob-haikei.svg';
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,24 @@
 <template>
-	<div :style="{ backgroundImage: `url(${wave1})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }">
-		<div class="flex flex-wrap items-center">
-			<div class="w-full lg:w-1/2">
-				<Anim loc="/anim/home.json" />
-			</div>
+	<div class="flex flex-wrap items-center">
+		<div class="w-full lg:w-1/2">
+			<Anim loc="/anim/home.json" />
+		</div>
 
-			<div class="w-full lg:w-1/2">
-				<h1 class="my-6 text-2xl lg:text-6xl font-bold text-gray-800">MOON { SYNTAX }</h1>
-				<p class="my-6 text-lg lg:text-2xl tracking-wide">
-					The decentralized organization that's shaking up the world of blockchain startups. With a focus on
-					developing, managing, and funding cutting-edge projects, our team of experts delivers unparalleled
-					support and guidance.
-				</p>
+		<div class="w-full lg:w-1/2">
+			<h1 class="my-6 text-2xl lg:text-6xl font-bold text-gray-800">MOON { SYNTAX }</h1>
+			<p class="my-6 text-lg lg:text-2xl tracking-wide">
+				The decentralized organization that's shaking up the world of blockchain startups. With a focus on developing,
+				managing, and funding cutting-edge projects, our team of experts delivers unparalleled support and guidance.
+			</p>
 
-				<div class="leading-6 uppercase font-semibold">
-					<div class="relative inline-block mr-3">
-						<butx textinput="Twitter" link="https://twitter.com/moonsyntax" />
-					</div>
-					<div class="relative inline-block mr-3">
-						<butx textinput="Blog" link="https://medium.com/moonsyntax" />
-					</div>
+			<div class="leading-6 uppercase font-semibold">
+				<div class="relative inline-block mr-3">
+					<butx textinput="Twitter" link="https://twitter.com/moonsyntax" />
+				</div>
+				<div class="relative inline-block mr-3">
+					<butx textinput="Blog" link="https://medium.com/moonsyntax" />
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
-
-<script setup>
-	import wave1 from '@/public/blob1.svg';
-</script>
