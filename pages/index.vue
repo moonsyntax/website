59d42cb5b6344e387cac8835810638bd0a1dd204
besoeasy <besoeasy@users.noreply.github.com>
2024-01-21 06:24:00 +0530

    Refactor homepage layout and add background image

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,31 @@
 <template>
-	<div class="flex flex-wrap items-center">
-		<div class="w-full lg:w-1/2">
-			<Anim loc="/anim/home.json" />
-		</div>
+	<div :style="{ backgroundImage: `url(${wave1})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }">
+		<div class="flex flex-wrap items-center">
+			<div class="w-full lg:w-1/2">
+				<Anim loc="/anim/home.json" />
+			</div>
 
-		<div class="w-full lg:w-1/2">
-			<h1 class="my-6 text-2xl lg:text-6xl font-bold text-gray-800 p-6">MOON { SYNTAX }</h1>
-			<p class="my-6 text-lg lg:text-2xl tracking-wide rounded-2xl bg-yellow-200 p-6">
-				The decentralized organization that's shaking up the world of blockchain startups. With a focus on developing,
-				managing, and funding cutting-edge projects, our team of experts delivers unparalleled support and guidance.
-			</p>
+			<div class="w-full lg:w-1/2">
+				<h1 class="my-6 text-2xl lg:text-6xl font-bold text-gray-800">MOON { SYNTAX }</h1>
+				<p class="my-6 text-lg lg:text-2xl tracking-wide">
+					The decentralized organization that's shaking up the world of blockchain startups. With a focus on
+					developing, managing, and funding cutting-edge projects, our team of experts delivers unparalleled
+					support and guidance.
+				</p>
 
-			<div class="p-6 leading-6 uppercase font-semibold">
-				<div class="relative inline-block mr-3">
-					<butx textinput="Twitter" link="https://twitter.com/moonsyntax" />
-				</div>
-				<div class="relative inline-block mr-3">
-					<butx textinput="Blog" link="https://medium.com/moonsyntax" />
+				<div class="leading-6 uppercase font-semibold">
+					<div class="relative inline-block mr-3">
+						<butx textinput="Twitter" link="https://twitter.com/moonsyntax" />
+					</div>
+					<div class="relative inline-block mr-3">
+						<butx textinput="Blog" link="https://medium.com/moonsyntax" />
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </template>
+
+<script setup>
+	import wave1 from '@/public/blob1.svg';
+</script>
