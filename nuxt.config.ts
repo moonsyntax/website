246d36bb579d60e362f92f310afc92ee5959cf62
besoeasy <besoeasy@users.noreply.github.com>
2024-01-24 08:58:22 +0530

    Add nitro configuration for prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  nitro: {
+    prerender: {
+      autoSubfolderIndex: false
+    }
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", '@nuxt/content'],
   vue: {
