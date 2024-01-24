9ded4ad2b1f23592b75712667e85d1b62d502b12
besoeasy <besoeasy@users.noreply.github.com>
2024-01-24 08:11:33 +0530

    Update contact information formatting

--- pages/contact.vue
+++ pages/contact.vue
@@ -12,12 +12,18 @@
 
 					<div class="w-full lg:w-2/4">
 						<div class="max-w-xs mx-auto mb-10">
-							<span class="block mb-6 text-5xl font-heading text-gray-700">Team</span>
-							<h4 class="text-3xl font-heading font-semibold">info@moonsyntax.com</h4>
+							<span class="block mb-6 text-xl lg:text-5xl font-heading text-gray-700"
+								>Team</span
+							>
+							<h4 class="text-xl lg:text-3xl font-heading font-semibold">
+								info@moonsyntax.com
+							</h4>
 						</div>
 						<div class="max-w-xs mx-auto">
-							<span class="block mb-6 text-5xl font-heading text-gray-700">Manager</span>
-							<h4 class="text-3xl font-heading font-semibold">x@moonsyntax.com</h4>
+							<span class="block mb-6 text-xl lg:text-5xl font-heading text-gray-700"
+								>Manager</span
+							>
+							<h4 class="text-xl lg:text-3xl font-heading font-semibold">x@moonsyntax.com</h4>
 						</div>
 					</div>
 				</div>
