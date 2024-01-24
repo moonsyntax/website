9fe6d1f8beffe8646159206f8445e608e116b0a6
besoeasy <besoeasy@users.noreply.github.com>
2024-01-24 05:55:14 +0530

    Add ContentRenderer and ContentRendererMarkdown components to [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,7 +1,18 @@
 <template>
 	<main>
-		<div class="bg-white max-w-4xl my-20 py-20 rounded-2xl"></div>
+		<div class="bg-white max-w-4xl my-20 py-20 rounded-2xl">
+			<ContentRenderer :value="data">
+				<h1>{{ data.title }}</h1>
+				<ContentRendererMarkdown :value="data" />
+			</ContentRenderer>
+		</div>
 	</main>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+	const router = useRouter();
+
+	const route = router.currentRoute.value.params.slug.join('/');
+
+	const { data } = await useAsyncData('page-data', () => queryContent(route).findOne());
+</script>
