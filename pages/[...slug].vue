98b035ed7829e47fd7af663a11a64cfa00ec8480
besoeasy <besoeasy@users.noreply.github.com>
2024-01-25 08:16:42 +0530

    Add error handling for missing data in page component

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,6 +5,10 @@
 
 	const { data } = await useAsyncData('page-data', () => queryContent(route).findOne());
 
+	if (!data.value) {
+		router.replace('/');
+	}
+
 	useSeoMeta({
 		title: () => data.value?.title,
 		description: () => data.value?.description,
