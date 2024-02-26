5481cf0203f49f562ad6439b1d636d6c2d776d67
besoeasy <besoeasy@users.noreply.github.com>
2024-02-27 04:38:59 +0530

    Update styling and hover effects in featdis component

--- components/featdis.vue
+++ components/featdis.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="max-w-sm mt-10">
-    <span class="block text-4xl font-bold text-yellow-500 mb-4">{{ id }}</span>
-    <h4 class="text-2xl font-bold text-white mb-4">{{ title }}</h4>
-    <p class="text-gray-500">{{ subtitle }}</p>
+    <span class="block text-4xl font-bold text-yellow-500 mb-4 hover:scale-125">{{ id }}</span>
+    <h4 class="text-2xl font-bold text-white mb-4 hover:text-yellow-500">{{ title }}</h4>
+    <p class="text-white tracking-wide">{{ subtitle }}</p>
   </div>
 </template>
 
