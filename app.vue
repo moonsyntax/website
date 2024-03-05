330a32574f527861557bc491c15263a421db9511
Aman Singh Katal <besoeasy@users.noreply.github.com>
2024-03-06 04:01:53 +0530

    bg:

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="min-h-screen text-white bg-black">
-    <div class="p-4 uppercase bg-yellow-300 text-slate-900">
+    <div class="p-4 uppercase bg-yellow-200 hover:bg-yellow-300 text-slate-900">
       <div class="container m-auto font-bold tracking-widest">
         <div class="flex items-center justify-center space-x-4 lg:space-x-6">
           <NuxtLink v-if="route.path !== '/'" to="/" class="navlinks"
